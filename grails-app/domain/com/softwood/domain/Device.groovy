b09829656552a4394bc2ac9bdb4ce3d23e701194
woodmawa <will.woodman@btinternet.com>
2019-01-14 17:58:35 +0000

    new classes

--- grails-app/domain/com/softwood/domain/Device.groovy
+++ grails-app/domain/com/softwood/domain/Device.groovy
@@ -1,13 +1,29 @@
 package com.softwood.domain
 
+/**
+ * logical construct to represent hardware or software that is performing a recognised role
+ * and has name and management identity
+ */
 class Device extends ManagedEntity {
+
+    ManagedEntity ci
+    Site site
+    Location location
+    NetworkDomain domain
+    Collection deviceRoles
 
     boolean isFreeStanding = false
     String licence = "none"
     String memory
     String storage
 
+    static hasMany = [deviceRoles: ResourceRoles]
+
     static constraints = {
+        ci nullable : false
+        site nullable:true
+        location nullable:true
+        domain nullable:true
         memory nullable:true
         storage nullable:true
     }
