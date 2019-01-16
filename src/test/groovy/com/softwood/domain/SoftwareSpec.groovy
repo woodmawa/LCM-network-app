2b04e42c0b48c271c3d234d82a88ac2b463e3b32
woodmawa <will.woodman@btinternet.com>
2019-01-16 17:07:09 +0000

    new classes

---
+++ src/test/groovy/com/softwood/domain/SoftwareSpec.groovy
@@ -0,0 +1,18 @@
+package com.softwood.domain
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class SoftwareSpec extends Specification implements DomainUnitTest<Software> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
