cac95268b54badce323ed2e2bdf4c571b4913d72
woodmawa <will.woodman@btinternet.com>
2019-01-29 22:41:53 +0000

    renamed integration tests - still failing

---
+++ src/test/groovy/com/softwood/service/JsonApiProcessorServiceSpec.groovy
@@ -0,0 +1,18 @@
+package com.softwood.service
+
+import grails.testing.services.ServiceUnitTest
+import spock.lang.Specification
+
+class JsonApiProcessorServiceSpec extends Specification implements ServiceUnitTest<JsonApiProcessorService>{
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
