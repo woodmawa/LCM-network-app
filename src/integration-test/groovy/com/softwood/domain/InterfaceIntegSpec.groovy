d349e8e6ae766de8cdb658dc57fd43057369bbda
woodmawa <will.woodman@btinternet.com>
2019-01-31 23:31:46 +0000

    add interface intgration test with sub interfaces

--- src/integration-test/groovy/com/softwood/domain/InterfaceIntegSpec.groovy
+++ src/integration-test/groovy/com/softwood/domain/InterfaceIntegSpec.groovy
@@ -48,5 +48,42 @@ class InterfaceIntegSpec extends Specification {
 
     }
 
+    void "create an interface with a subinterface  and link to pe "() {
+
+        given: "existing pe  "
+
+        Device pe = Device.get(2)
+        assert pe.linkedTo.size() == 0
+
+        when: "add new interface to pe"
+
+        Interface mif = new Interface (name:"eth0", ipAddress: "10.5.25.34", port:"8080", interfaceStatus: Interface.InterfaceState.Up, bandwidth:"10 Gbit/s")
+        mif.validate() == true
+        if (mif.hasErrors()) {
+            mif.errors.fieldErrors.each { FieldError fe -> println "field:"+  fe.field + " rejected : " + fe.rejectedValue}
+        }
+        pe.addToInterfaces(mif)
+
+        //needs the flush:true to force the write to the db!
+        //https://spring.io/blog/2010/07/02/gorm-gotchas-part-2/
+        pe.save (flush:true)
+
+        Interface subif = new Interface (name:"eth0/1", ipAddress: "10.5.25.25", port:"80", interfaceStatus: Interface.InterfaceState.Up, bandwidth:"1 Gbit/s")
+
+        mif.addToSubInterfaces(subif)
+        mif.save(flush:true)
+
+        then:
+        pe.interfaces.size() == 1
+        pe.interfaces[0].name =="eth0"
+        mif.id == 1
+        Interface.count() == 2
+        mif.subInterfaces.size() == 1
+        subif.id == 2
+        subif.parent == mif
+
+
+    }
+
 
 }
